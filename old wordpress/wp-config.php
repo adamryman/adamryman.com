f90d92040014087393e21abe0becc7fd5c58483a
Adam Ryman <adamryman@Adams-MacBook-Pro.local>
2012-08-05 03:30:06 -0700

    Starting using git more

---
+++ old wordpress/wp-config.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_NAME', 'adamryman_blog');
+define('DB_USER', 'adamryman');
+define('DB_PASSWORD', '4KP3msn7WDYS5caL');
+define('DB_HOST', 'goldmine');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('AUTH_KEY', 'sjhdiuashdiuahsd20593');
+define('SECURE_AUTH_KEY', 'oklasdjoiasdjoij14424');
+define('LOGGED_IN_KEY', 'sadjhjoasdjoi4583');
+define('NONCE_KEY', 'asdijoasdjioidj3780');
+define ('WPLANG', '');
+if ( !defined('ABSPATH') )
+define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
+?>
