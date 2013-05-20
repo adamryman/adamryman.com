b900cedbad97621350d6893a60e437acaee38262
Adam Ryman <adamryman@gmail.com>
2013-05-19 22:46:51 -0700

    Added a small amount of text to about, plus restyling

--- about.php
+++ about.php
@@ -1,7 +1,13 @@
 <?php include 'temps/header.php'; ?>
 				<div class="lightbox" id="lightbox-about">
 					<h1>About</h1>
-					<span id="subtext">Value Creator</span>
+					<p> Hey there!</p>
+
+					<p> My name is Adam, I try to create value where there was none before.</p>
+
+					<p>Curently I run <a href="http://studentrnd.org" target="_blank">StudentRND</a> and 
+						<a href="http://codeday.org" target="_blank">CodeDay</a>.
+
 				</div>
 			</div>
 		</div>
