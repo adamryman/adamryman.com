6f7cbc878ae2705dd071918b5886bf64b5d33fe6
Adam Ryman <adamryman@gmail.com>
2016-02-21 17:34:09 -0800

    Remove about and add resume and vertical bar

--- temps/header.php
+++ temps/header.php
@@ -9,7 +9,8 @@
 			<div id="background">
 				<div id="nav-bar"><ul>
 					<li id="nav-name"><a href="index.php">Adam Ryman</li>
-					<li><a href="about.php">About</a></li>
+					<li><a href="/files/resume.pdf" target="_blank">Resume</a></li>
+					<li>|</li>
 					<li><a href="http://github.com/adamryman" target="_blank">GitHub</a></li>
 					<li><a href="http://twitter.com/adamhryman" target="_blank">Twitter</a></li>
 					<li><a href="www.linkedin.com/in/adamryman" target="_blank">LinkedIn</a></li>
