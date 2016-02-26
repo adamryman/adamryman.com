47a48dcad0a85d4865e6dc5d9de608e2b2365370
Adam Ryman <adamryman@gmail.com>
2016-02-26 15:49:53 -0800

    Remove php, fix rendering for DOCTYPE html

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'temps/header.php'; ?>
-				<div class="lightbox" id="lightbox-home">
-					<span id="name">Adam Ryman</span>
-					<span id="subtext">Value Creator</span>
-				</div>
-			</div>
-<?php include 'temps/footer.php'; ?>
